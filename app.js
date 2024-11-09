a899586d3995191ba1e4a8b98915e0d0c2e1a9ac
Navneet Singh Solanki <navneetsinghsolanki2580@gmail.com>
2024-11-09 21:25:08 +0530

    typing handler done

--- app.js
+++ app.js
@@ -71,6 +71,9 @@ io.on("connection", (socket) => {
     console.log("User Joined Room", room);
   });
 
+  socket.on("typing", (room) => socket.in(room).emit("typing"));
+  socket.on("stop-typing", (room) => socket.in(room).emit("stop-typing"));
+
   socket.on("new-message", (newMessageReceived) => {
     var chat = newMessageReceived.chat;
 
@@ -82,5 +85,10 @@ io.on("connection", (socket) => {
       if (user._id == newMessageReceived.senderId._id) return;
       socket.in(user._id).emit("message-received", newMessageReceived);
     });
+  });
+
+  socket.off("setup", () => {
+    console.log("user disconnected");
+    socket.leave(userData._id);
   });
 });
